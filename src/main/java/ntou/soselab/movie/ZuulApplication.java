9e1e3d7a0b0ebf089f55615b3eb46b1d709ca003
Bernie <b4456609@gmail.com>
2017-03-10 13:19:15 +0800

    fix zuul setting

--- src/main/java/ntou/soselab/movie/ZuulApplication.java
+++ src/main/java/ntou/soselab/movie/ZuulApplication.java
@@ -2,9 +2,9 @@ package ntou.soselab.movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulServer
+@EnableZuulProxy
 @SpringBootApplication
 public class ZuulApplication {
 
