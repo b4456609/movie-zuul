c183754a2fc1c53eb54d585022ea03e65d8e94f6
Bernie <b4456609@gmail.com>
2017-03-10 12:27:19 +0800

    enable zuul server

--- src/main/java/ntou/soselab/movie/ZuulApplication.java
+++ src/main/java/ntou/soselab/movie/ZuulApplication.java
@@ -2,7 +2,9 @@ package ntou.soselab.movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
+@EnableZuulServer
 @SpringBootApplication
 public class ZuulApplication {
 
